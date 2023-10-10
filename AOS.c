4d1cfc91af7350b0b0ed09f012a79753b90cad35
Vidyasri17 <147472710+Vidyasri17@users.noreply.github.com>
2023-10-10 11:28:26 +0530

    Add files via upload

---
+++ AOS.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a , p;
+	a = 6;
+	p = 4*a;
+	printf("%i",p);
+	return 0;
+}
---
+++ POS.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a , p;
+	a=88;
+	p = 4*a;
+	printf("%i",p);
+	return 0;
+}
